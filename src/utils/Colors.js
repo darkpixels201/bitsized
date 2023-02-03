fdb801098c8117449e52616585208d787beecea8
darkpixels201 <darkpixels201@gmail.com>
2023-02-03 15:46:45 +0500

    Assets

---
+++ src/utils/Colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  purple:"#B388EB",
+  red:"#FD4120",
+  lightRed:"#FD726A",
+  black:"#101113",
+  black1:"141414",
+  darkGrey:"#535D66",
+  lightGrey:"#818386",
+  white:"#FFFFFF",
+  white1:"#F2F5FA",
+};
---
+++ src/utils/CommonStyles.js
@@ -0,0 +1,79 @@
+
+  // import {
+  
+  //   Platform, View
+  // } from 'react-native'
+  import styled from "styled-components";
+  // import { colors } from './Colors';
+  // import colors from './colors';
+  // const commonStyles = ScaledSheet.create({
+  //   container: {
+    
+  //     flexDirection: 'column',
+  //     padding: 10,
+  //     flex: 1,
+  //   },
+ 
+  //   rowMain: {
+  //     justifyContent: 'center',
+  //     alignItems: 'center',
+  //     flexDirection: 'row',
+  //   },
+  
+  //   padding: {
+  //     paddingHorizontal: 20,
+  //   },
+  
+  //   justify: {
+  //     justifyContent: 'space-between',
+  //     alignItems: 'center',
+  //     flexDirection: 'row',
+  //   },
+  //   iconContainer: {
+  //     width: moderateScale(25),
+  //     height: verticalScale(25),
+  //   },
+  //   img: {
+  //     width: '100%',
+  //     height: '100%',
+  //   },
+    
+  //   containerCenter: {
+  //     justifyContent: 'center',
+  //     alignItems: 'center',
+  //   },
+
+  //   IosPadding:{
+  //     flex: 1,paddingTop:Platform.OS=="ios"?verticalScale(40):5,
+  //     paddingHorizontal:moderateScale(15),
+  //   },
+  //   IosContainer:{
+  //     flex: 1,paddingTop:Platform.OS=="ios"?verticalScale(40):5,
+  //   },
+
+  // });
+  export const row = {
+    display: "flex",
+    flexDirection: "row",
+  };
+  export const center = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  export const noUnderline = {
+    textDecoration: "none",
+  };
+  // export const PH10 = styled(View, {
+  //   paddingHorizontal: scale(10),
+  // });
+  // export const PH5 = styled(View, {
+  //   paddingHorizontal: scale(5),
+  // });
+  // export const PH20 = styled(View, {
+  //   paddingHorizontal: scale(20),
+  // });
+
+  
+  // export default commonStyles;
+  
